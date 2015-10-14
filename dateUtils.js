174fb624fb3246d1ea3afc01e0ee1648e39c9d77
Adam Schmalhofer <Adam.Schmalhofer@gmx.de>
2015-10-14 16:31:21 +0200

    Put day change in sync with duolingo's server

--- dateUtils.js
+++ dateUtils.js
@@ -1,7 +1,7 @@
 function isToday(date) {
-	let today = new Date();
-	let current_day = today.getDate();
-	let current_month = today.getMonth();
-	let current_year = today.getFullYear();
-	return date.getDate() == current_day && date.getMonth() == current_month && date.getFullYear() == current_year;
+	let today = new Date(new Date().getTime() - 4 * 60 * 60 * 1000);
+	let current_day = today.getUTCDate();
+	let current_month = today.getUTCMonth();
+	let current_year = today.getUTCFullYear();
+	return date.getUTCDate() == current_day && date.getUTCMonth() == current_month && date.getUTCFullYear() == current_year;
 }
