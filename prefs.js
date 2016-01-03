58508508fe673726425ac025343bc6e51266b54a
david <david.bonnal@gmail.com>
2016-01-03 10:11:43 +0100

    fix + wrapping

--- prefs.js
+++ prefs.js
@@ -13,7 +13,10 @@ DuolingoStatusSettingsWidget.prototype = {
 
 	_init: function() {		
 	
-		this.vbox = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, spacing: 6});
+		this.vbox = new Gtk.Box({
+			orientation: Gtk.Orientation.VERTICAL, 
+			spacing: 6
+		});
 		let stack = new Gtk.Stack({
             transition_type: Gtk.StackTransitionType.SLIDE_LEFT_RIGHT,
             transition_duration: 500,
@@ -29,39 +32,68 @@ DuolingoStatusSettingsWidget.prototype = {
             stack: stack
         });
         
+        /***************************************
+			Main section
+		***************************************/
+        
 		this._grid = new Gtk.Grid({ orientation: Gtk.Orientation.VERTICAL,
             row_spacing: 4,
-            column_spacing: 4 });
+            column_spacing: 4 
+        });
 
 		/* Username field */
-		let username_label = new Gtk.Label({label: 'Username', halign: Gtk.Align.START});
-		username_field = new Gtk.Entry({hexpand: true, halign: Gtk.Align.FILL});
+		let username_label = new Gtk.Label({
+			label: 'Username', 
+			halign: Gtk.Align.START
+		});
+		username_field = new Gtk.Entry({
+			hexpand: true, 
+			halign: Gtk.Align.FILL
+		});
 		username_field.text = Settings.get_string('username');
 		this._grid.attach(username_label, 0, 0, 1, 1);
 		this._grid.attach(username_field, 1, 0, 3, 1);
 		
 		/* Hide icon when daily goal is reached */
-		let hide_icon_label = new Gtk.Label({label: 'Hide icon when daily goal is reached', hexpand: true, halign: Gtk.Align.START});
+		let hide_icon_label = new Gtk.Label({
+			label: 'Hide icon when daily goal is reached', 
+			hexpand: true, 
+			halign: Gtk.Align.START
+		});
 		this._grid.attach(hide_icon_label, 0, 1, 3, 1);
-		let hide_icon_switch = new Gtk.Switch({active: Settings.get_boolean('hide-when-daily-goal-reached'), halign: Gtk.Align.END});
+		
+		let hide_icon_switch = new Gtk.Switch({
+			active: Settings.get_boolean('hide-when-daily-goal-reached'), 
+			halign: Gtk.Align.END
+		});
 		hide_icon_switch.connect('notify::active', function() {
 			Settings.set_boolean('hide-when-daily-goal-reached', hide_icon_switch.active);
+			enable_change_icon_color_label_switch.set_sensitive(!hide_icon_switch.active);
 		});
 		this._grid.attach(hide_icon_switch, 2, 1, 1, 1);
 		
 		/* Change icon color when daily goal is reached */
-		let change_icon_color_label = new Gtk.Label({label: 'Icon color when daily goal is reached', hexpand: true, halign: Gtk.Align.START});
+		let change_icon_color_label = new Gtk.Label({
+			label: 'Change icon color when daily goal is reached', 
+			hexpand: true, 
+			halign: Gtk.Align.START
+		});
 		this._grid.attach(change_icon_color_label, 0, 2, 2, 1);
 		
-		let initial_active = Settings.get_boolean('change-icon-color-when-daily-goal-reached');
-		let enable_change_icon_color_label_switch = new Gtk.Switch({active: initial_active, halign: Gtk.Align.END});
+		let enable_change_icon_color_label_switch = new Gtk.Switch({
+			active: Settings.get_boolean('change-icon-color-when-daily-goal-reached'),
+			sensitive: !hide_icon_switch.active,
+			halign: Gtk.Align.END
+		});
 		enable_change_icon_color_label_switch.connect('notify::active', function() {
 			Settings.set_boolean('change-icon-color-when-daily-goal-reached', enable_change_icon_color_label_switch.active);
 			color_picker_button.set_sensitive(enable_change_icon_color_label_switch.active);
 		});
 		this._grid.attach(enable_change_icon_color_label_switch, 2, 2, 1, 1);
 		
-		let color_picker_button = new Gtk.ColorButton({halign: Gtk.Align.CENTER});
+		let color_picker_button = new Gtk.ColorButton({
+			halign: Gtk.Align.CENTER
+		});
 		color_picker_button.set_use_alpha(false);
 		let rgba = new Gdk.RGBA();
 		rgba.parse(Settings.get_string('icon-color-when-daily-goal-reached'));
@@ -69,14 +101,20 @@ DuolingoStatusSettingsWidget.prototype = {
 		color_picker_button.connect('color-set', function() {
 			Settings.set_string('icon-color-when-daily-goal-reached', color_picker_button.rgba.to_string());
 		});
-		color_picker_button.set_sensitive(initial_active);
+		color_picker_button.set_sensitive(Settings.get_boolean('change-icon-color-when-daily-goal-reached'));
 		this._grid.attach(color_picker_button, 3, 2, 1, 1);
 		
 		/* Change icon color when daily goal is not reached */
-		change_icon_color_label = new Gtk.Label({label: 'Icon color when daily goal is reached', hexpand: true, halign: Gtk.Align.START});
+		change_icon_color_label = new Gtk.Label({
+			label: 'Change icon color when daily goal is not reached', 
+			hexpand: true, 
+			halign: Gtk.Align.START
+		});
 		this._grid.attach(change_icon_color_label, 0, 3, 2, 1);
 		
-		let color_picker_button_not_reached = new Gtk.ColorButton({halign: Gtk.Align.CENTER});
+		let color_picker_button_not_reached = new Gtk.ColorButton({
+			halign: Gtk.Align.CENTER
+		});
 		color_picker_button_not_reached.set_use_alpha(false);
 		let rgba = new Gdk.RGBA();
 		rgba.parse(Settings.get_string('icon-color-when-daily-goal-not-reached'));
@@ -88,12 +126,26 @@ DuolingoStatusSettingsWidget.prototype = {
 		
 		stack.add_titled(this._grid, "main", "Main");
 		
-		this._grid = new Gtk.Grid({ orientation: Gtk.Orientation.VERTICAL,
+		
+		/***************************************
+			Browser section
+		***************************************/
+		
+		this._grid = new Gtk.Grid({ 
+			orientation: Gtk.Orientation.VERTICAL,
             row_spacing: 4,
-            column_spacing: 4 });
+            column_spacing: 4 
+        });
+            
         /* Default browser switch */
-        let default_browser_label = new Gtk.Label({label: 'Default browser', halign: Gtk.Align.START});
-		_default_browser_switch = new Gtk.Switch({active: Settings.get_boolean('use-default-browser'), halign: Gtk.Align.END});
+        let default_browser_label = new Gtk.Label({
+        	label: 'Default browser', 
+        	halign: Gtk.Align.START
+        });
+		_default_browser_switch = new Gtk.Switch({
+			active: Settings.get_boolean('use-default-browser'), 
+			halign: Gtk.Align.END
+		});
 		this._grid.attach(default_browser_label, 0, 0, 1, 1);
 		this._grid.attach(_default_browser_switch, 1, 0, 3, 1);
 
@@ -101,14 +153,17 @@ DuolingoStatusSettingsWidget.prototype = {
 		let custom_browser_label = new Gtk.Label({
 			label: 'Browser command', 
 			halign: Gtk.Align.START,
-			sensitive: !Settings.get_boolean('use-default-browser')});
+			sensitive: !Settings.get_boolean('use-default-browser')
+		});
 		_custom_browser_field = new Gtk.Entry({
 			hexpand: true, 
 			halign: Gtk.Align.FILL,
-			sensitive: !Settings.get_boolean('use-default-browser')});
+			sensitive: !Settings.get_boolean('use-default-browser')
+		});
 		let app_chooser_button = new Gtk.AppChooserButton({
 			content_type:"text/html",
-			sensitive: !Settings.get_boolean('use-default-browser')});
+			sensitive: !Settings.get_boolean('use-default-browser')
+		});
 		app_chooser_button.set_show_dialog_item(true)
 		app_chooser_button.set_active(Settings.get_int('app-chooser-active-index'));
 		this._grid.attach(custom_browser_label, 0, 1, 1, 1);
